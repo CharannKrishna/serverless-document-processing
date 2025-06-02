2c50221562e0a04e643aa015629d1c2219875748
Charan Krishna <charannori.0401@gmail.com>
2025-06-02 18:17:31 +0530

    Initial commit

---
+++ test/serverless-document-processing.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessDocumentProcessing from '../lib/serverless-document-processing-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-document-processing-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessDocumentProcessing.ServerlessDocumentProcessingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
